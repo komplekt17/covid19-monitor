34b82293f2ed3c402f0de574dce874323124522a
komplekt17 <komplekt_17@mail.ru>
2020-05-10 15:40:50 +0500

    Updates

---
+++ precache-manifest.34780c27f8197d5c8fc03fa6f1097aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86506f7be55e3bb175e4b1b16d6ea0f",
+    "url": "/covid19-monitor/index.html"
+  },
+  {
+    "revision": "37b8d4b15db2163f4f55",
+    "url": "/covid19-monitor/static/css/main.f87d5c85.chunk.css"
+  },
+  {
+    "revision": "462b00eba0aba16e82cb",
+    "url": "/covid19-monitor/static/js/2.8a3afaca.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-monitor/static/js/2.8a3afaca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b8d4b15db2163f4f55",
+    "url": "/covid19-monitor/static/js/main.12499632.chunk.js"
+  },
+  {
+    "revision": "3d7dd17fc6bc6969d2b2",
+    "url": "/covid19-monitor/static/js/runtime-main.cfacb3cd.js"
+  },
+  {
+    "revision": "758cb721bf5b37d946eea89f074ff794",
+    "url": "/covid19-monitor/static/media/covid-full.758cb721.png"
+  }
+]);
